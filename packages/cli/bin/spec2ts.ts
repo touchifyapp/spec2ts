2b2c0a1d98b78457520fff2c116b7f8d0e5c5df5
Maxime LUCE <maxime@touchify.co>
2020-04-24 21:20:21 +0200

    refactor(global): improve project architecture
    BREAKING CHANGE: force v1

--- packages/cli/bin/spec2ts.ts
+++ packages/cli/bin/spec2ts.ts
@@ -1,2 +1,26 @@
 #!/usr/bin/env node
-import("../cli");
+
+import * as yargs from "yargs";
+
+import * as jsonschema from "@spec2ts/jsonschema/cli/command";
+import * as openapi from "@spec2ts/openapi/cli/command";
+
+yargs
+    .command(
+        jsonschema.usage.replace("$0", "jsonschema"),
+        jsonschema.describe,
+        jsonschema.builder,
+        jsonschema.handler
+    )
+
+    .command(
+        openapi.usage.replace("$0", "openapi"),
+        openapi.describe,
+        openapi.builder,
+        openapi.handler
+    )
+
+    .help("help", "Show help usage")
+    .demandCommand()
+
+    .argv;
