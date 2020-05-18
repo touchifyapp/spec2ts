7c9814c9ce0be84c14b1c6c8d86834fea301d005
Maxime LUCE <maxime@touchify.co>
2020-05-18 21:17:57 +0200

    feat(cli): add openapi-client cli

--- packages/cli/bin/spec2ts.ts
+++ packages/cli/bin/spec2ts.ts
@@ -4,6 +4,7 @@ import * as yargs from "yargs";
 
 import * as jsonschema from "@spec2ts/jsonschema/cli/command";
 import * as openapi from "@spec2ts/openapi/cli/command";
+import * as openapiClient from "@spec2ts/openapi-client/cli/command";
 
 yargs
     .command(
@@ -18,6 +19,13 @@ yargs
         openapi.describe,
         openapi.builder,
         openapi.handler
+    )
+
+    .command(
+        openapiClient.usage.replace("$0", "openapi-client"),
+        openapiClient.describe,
+        openapiClient.builder,
+        openapiClient.handler
     )
 
     .help("help", "Show help usage")
